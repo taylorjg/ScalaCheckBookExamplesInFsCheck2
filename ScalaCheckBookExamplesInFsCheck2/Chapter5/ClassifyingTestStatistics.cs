62f6af632e673d9102d11b760c23bcda4aaca47e
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-06-27 12:18:48 +0100

    Inlined vars in Classify exsample

--- ScalaCheckBookExamplesInFsCheck2/Chapter5/ClassifyingTestStatistics.cs
+++ ScalaCheckBookExamplesInFsCheck2/Chapter5/ClassifyingTestStatistics.cs
@@ -9,13 +9,12 @@ namespace ScalaCheckBookExamplesInFsCheck2.Chapter5
         [Property]
         public Property P(int n)
         {
-            var p1 = n + n == 2*n;
-            var p2 = p1.Classify(n%2 == 0, "even");
-            var p3 = p2.Classify(n%2 != 0, "odd");
-            var p4 = p3.Classify(n < 0, "neg");
-            var p5 = p4.Classify(n >= 0, "pos");
-            var p6 = p5.Classify(Math.Abs(n) > 50, "large");
-            return p6;
+            return (n + n == 2*n)
+                .Classify(n%2 == 0, "even")
+                .Classify(n%2 != 0, "odd")
+                .Classify(n < 0, "neg")
+                .Classify(n >= 0, "pos")
+                .Classify(Math.Abs(n) > 50, "large");
         }
     }
 }
