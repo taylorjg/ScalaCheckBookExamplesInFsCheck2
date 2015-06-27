f1c5709b9126cedfd0916d5dd435b5c191ed392d
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2015-06-27 20:56:15 +0100

    Added second throws example

--- ScalaCheckBookExamplesInFsCheck2/Chapter5/Throws.cs
+++ ScalaCheckBookExamplesInFsCheck2/Chapter5/Throws.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using FsCheck;
 using FsCheck.Xunit;
 
@@ -10,6 +11,17 @@ namespace ScalaCheckBookExamplesInFsCheck2.Chapter5
         public Property PropDivByZero(int n)
         {
             return Prop.throws<DivideByZeroException, int>(new Lazy<int>(() => n/(n - n)));
+        }
+
+        [Property]
+        public Property PropListBounds(IList<string> xs, int i)
+        {
+            var inside = i >= 0 && i < xs.Count;
+            var p1 = inside.ToProperty();
+            var p2 = Prop.throws<ArgumentOutOfRangeException, string>(new Lazy<string>(() => xs[i]));
+            return p1.Or(p2)
+                .Classify(inside, "inside")
+                .Classify(!inside, "outside");
         }
     }
 }
